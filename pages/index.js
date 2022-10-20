3d8b658994197496626ab79fb2daa4b8c955d952
Tony Tong <52387376+qwerfd2@users.noreply.github.com>
2022-10-20 15:47:25 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import MainPage from "./MainPage";
 import firebaseDemo from "./firebaseDemo";
+import Auth from "./Auth";
 
 export default function Home() {
 
   return (
-   <React.Fragment>
-     <MainPage/>
-     {/*<firebaseDemo/>*/}
-   </React.Fragment>
+      <React.Fragment>
+        <Auth/>
+      </React.Fragment>
   );
 
 }
