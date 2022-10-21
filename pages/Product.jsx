c3500340b0dec88ec11bb1a8041fc98bfb4ef427
Tony Tong <52387376+qwerfd2@users.noreply.github.com>
2022-10-20 20:00:22 -0500

    Update Product.jsx

--- pages/Product.jsx
+++ pages/Product.jsx
@@ -5,143 +5,107 @@ import Image from 'next/image'
 
 import Button from '@mui/material/Button';
 import * as React from 'react';
-import {TextField, FormControl, InputLabel, Select, MenuItem, NativeSelect} from "@mui/material";
+import {TextField, FormControl, InputLabel, Select, MenuItem, NativeSelect, Grid} from "@mui/material";
 import {useState} from "react";
 export default function Auth() {
 
     //Const for User Login
     const [product, setProduct] = useState({
-        lister: '',
-        productName: '',
-        productDescription: '',
-        productImagePath: ["/image.jpg"],
-        timeCreated: '',
+        lister: '', //Lister should be acquired in the backend.
+        name: '',
+        description: '',
+        imagePath: ["/image.jpg"], //Dummy path to images for now. Don't know where to put the file yet.
+        timeCreated: '',    //This should be acquired in the backend.
         transactionType: '',
-        sellProgress: ''
+        sellProgress: ''   //This should be defaulted to 0 in the backend.
     });
 
 
     return (
         <React.Fragment>
             <h2>Submit a Product</h2>
-            <TextField
-                id="outlined-basic"
-                label="Product name"
-                style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center'
-                }}
-                onChange={(v) => {
-                    setProduct({
-                        ...product,
-                        productName: v.target.value,
-                    })
-                }}
-            /><br></br>
-            <TextField
-                multiline={true}
-                rows={3}
-                id="outlined-basic"
-                label="Product description"
-                onChange={(v) => {
-                    setProduct({
-                        ...product,
-                        productDescription:v.target.value,
-                    })
-                }}
-            /><br></br>
-
-            <FormControl fullWidth>
-                <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                    Method of Payment
-                </InputLabel>
-                <NativeSelect
-                    defaultValue={0}
-                    inputProps={{
-                        name: 'paymentType',
-                        id: 'uncontrolled-native',
-                    }}
-                >
-                    <option value={0}>Item Swap</option>
-                    <option value={1}>Venmo</option>
-                    <option value={2}>CashApp</option>
-                    <option value={3}>Other</option>
-                </NativeSelect>
-            </FormControl> <br></br>
-
-            <TextField
-                id="outlined-basic"
-                label="New Username"
-                onChange={(v) => {
-                    setRegister({
-                        ...register,
-                        username:v.target.value,
-                    })
-                }}
-            /><br></br>
-            <TextField
-                id="outlined-basic"
-                label="New Password"
-                onChange={(v) => {
-                    setRegister({
-                        ...register,
-                        password:v.target.value,
-                    })
-                }}
-            /><br></br>
-            <TextField
-                id="outlined-basic"
-                label="First Name"
-                onChange={(v) => {
-                    setRegister({
-                        ...register,
-                        firstname:v.target.value,
-                    })
-                }}
-            /><br></br>
-            <TextField
-                id="outlined-basic"
-                label="Last Name"
-                onChange={(v) => {
-                    setRegister({
-                        ...register,
-                        lastname:v.target.value,
-                    })
-                }}
-            /><br></br>
-            <TextField
-                id="outlined-basic"
-                label="Email"
-                onChange={(v) => {
-                    setRegister({
-                        ...register,
-                        email:v.target.value,
-                    })
-                }}
-            /><br></br>
-            <TextField
-                id="outlined-basic"
-                label="Phone number"
-                onChange={(v) => {
-                    setRegister({
-                        ...register,
-                        phone:v.target.value,
-                    })
-                }}
-            /><br></br><br></br>
-
-            <Button
-                variant="contained"
-                onClick={(e) => {
-                    e.preventDefault();
-                    //console.log(user);
-                    alert("username" + register.username + "  password" + register.password + "  first name" + register.firstname+ "  last name" + register.lastname + "  email" + register.email+ "  phone" + register.phone);
-                    // sent to backend and firebase
-                }}
-            >Sign Up</Button><br></br><br></br><br></br>
-
+            <Grid container
+                    justifyContent="center"
+                    alignItems="center"
+                    spacing={{xl: 2}}
+                    columns={{xl: 2}}
+            >
+                <Grid item>
+                    <TextField
+                        id="outlined-basic"
+                        label="Product name"
+                        onChange={(v) => {
+                            setProduct({
+                                ...product,
+                                name: v.target.value,
+                            })
+                        }}
+                    />
+                </Grid>
+                <Grid item>
+                    <TextField
+                        multiline={true}
+                        rows={3}
+                        id="outlined-basic"
+                        label="Product description"
+                        onChange={(v) => {
+                            setProduct({
+                                ...product,
+                                description:v.target.value,
+                            })
+                        }}
+                    />
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        component="label"
+                    >
+                        Upload File
+                        <input
+                            type="file"
+                            hidden
+                        />
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <FormControl fullWidth>
+                        <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                            Method of Payment
+                        </InputLabel>
+                        <NativeSelect
+                            defaultValue={0}
+                            inputProps={{
+                                name: 'paymentType',
+                                id: 'uncontrolled-native',
+                            }}
+                            onChange={(v) => {
+                                setProduct({
+                                    ...product,
+                                    transactionType:v.target.value,
+                                })
+                            }}
+                        >
+                            <option value={0}>Select...</option>
+                            <option value={1}>Item Swap</option>
+                            <option value={2}>Venmo</option>
+                            <option value={3}>CashApp</option>
+                            <option value={4}>Other</option>
+                        </NativeSelect>
+                    </FormControl>
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            //console.log(user);
+                            alert("product name: " + product.name + " description: " + product.description + " Image list: "+ JSON.stringify(product.imagePath) + " transaction type: " + product.transactionType);
+                            // sent to backend and firebase
+                        }}
+                    >List the Item!</Button>
+                </Grid>
+            </Grid>
         </React.Fragment>
     )
 }
-
