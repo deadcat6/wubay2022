5f9b7d7d29dd6e49b0222f2f82003072c030a2de
deadcat6 <89941927+deadcat6@users.noreply.github.com>
2022-10-22 12:30:22 -0500

    fixed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,8 +10,11 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_SECRET,
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+
+      // clientId: process.env.GOOGLE_CLIENT_ID,
+      // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: "415414660701-8hs7mbjbmcmdu3v267sbcscnq02qu4a7.apps.googleusercontent.com",
+      clientSecret: "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",
     })
     // ...add more providers here
   ],
