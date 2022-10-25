f690affca529d6d2c45720eeb0a6820aa44f058d
michaelcafiero99 <78000917+michaelcafiero99@users.noreply.github.com>
2022-10-24 22:29:21 -0500

    by id fixed?

--- firebase/get_product_by_id.js
+++ firebase/get_product_by_id.js
@@ -1,17 +1,25 @@
-import {collection, getDocs, getFirestore, query, where,} from "firebase/firestore";
+import {collection, getDocs, getDoc,getFirestore, query, where,doc} from "firebase/firestore";
 import {app, database} from "./firebaseConfig";
 
 async function get_product_by_id(id) {
 
     const db = getFirestore(app)
-    const quer = query(collection(db, "products"), where("id", "==", id));
-    let product = {}
-    const querySnapshot = await getDocs(quer);
-    querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
-    product = doc.data()
-     })
-    return product
+    //const quer = query(collection(db, "products"), where("id", "==", id));
+    const docRef = doc(db, "products", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      console.log("Document data:", docSnap.data());
+    }
+    return docSnap.data()
+
+    //let product = {}
+   // const querySnapshot = await getDocs(quer);
+    //querySnapshot.forEach((doc) => {
+   // console.log(doc.id, " => ", doc.data());
+    //product = doc.data()
+    // })
+   // return product
 
 
 }
