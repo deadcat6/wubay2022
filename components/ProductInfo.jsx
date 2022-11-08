1bf7ec84548f0e79d6318713a3240485fb17818e
deadcat6 <89941927+deadcat6@users.noreply.github.com>
2022-11-08 17:30:01 -0600

    bug fixed

--- components/ProductInfo.jsx
+++ components/ProductInfo.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import {Avatar, Box, Button, Grid, Rating} from "@mui/material";
+import {Avatar, Box, Button, Dialog, Grid, Rating} from "@mui/material";
 import {H1, H2, H3, H6, Small} from "./Typography";
-import Link from "next/link";
 import ImageViewer from "react-simple-image-viewer";
 import {useRouter} from "next/router";
 import React, {useCallback, useState} from "react"; // import ImageViewer from "react-simple-image-viewer";
@@ -10,6 +9,7 @@ import {bgcolor, border, borderRadius, compose, sizing, spacing, styled, typogra
 import NextImage from "next/image";
 import emailjs from "@emailjs/browser";
 import {useSession} from "next-auth/react";
+import Login from "./Login";
 
 const LazyImage = styled(({borderRadius, ...rest}) => (
   <NextImage {...rest} />
@@ -30,7 +30,8 @@ BazaarRating.defaultProps = {
 // ================================================================
 const ProductInfo = ({product, id}) => {
   const {data: session} = useSession()
-
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const toggleDialog = () => setDialogOpen(!dialogOpen);
   //console.log(product);
   const imagePath = product.imagePath;
   //console.log(imagePath);
@@ -39,8 +40,15 @@ const ProductInfo = ({product, id}) => {
   const [isViewerOpen, setIsViewerOpen] = useState(false);
   const [currentImage, setCurrentImage] = useState(0);
   const [selectedImage, setSelectedImage] = useState(0);
-
+  const checkSessions = () => {
+    if (!session) {
+      setDialogOpen(true);
+      return false;
+    }
+    return true;
+  }
   const handleContact = (e) => {
+    if (checkSessions()) {
       e.preventDefault();
       let emailData = {
         to_name: product.username,
@@ -64,6 +72,7 @@ const ProductInfo = ({product, id}) => {
             //console.log(error.text);
           });
       }
+    }
   }
 
   const handleImageClick = (ind) => () => {
@@ -79,156 +88,168 @@ const ProductInfo = ({product, id}) => {
   };
 
   async function orderHandler() {
-    const response = await fetch('/api/user/addOrder', {
-      method: 'POST',
-      body: JSON.stringify({
-        userId: session.user.id,
-        productId: id,
-      }),
-      headers: {'Content-Type': 'application/json'}
-
-    });
-    await response.json();
-    await router.push('/user/orders')
+    if (checkSessions()) {
+      const response = await fetch('/api/user/addOrder', {
+        method: 'POST',
+        body: JSON.stringify({
+          userId: session.user.id,
+          productId: id,
+        }),
+        headers: {'Content-Type': 'application/json'}
+      });
+      await response.json();
+      await router.push('/user/orders')
+    }
   }
+
   return (
-    <Box width="100%">
-      <Grid container spacing={3} justifyContent="space-around">
-        <Grid item md={6} xs={12} alignItems="center">
-          <FlexBox justifyContent="center" mb={6}>
-            <LazyImage
-              width={300}
-              alt={product.title}
-              height={300}
-              loading="eager"
-              objectFit="contain"
-              src={imagePath[selectedImage]}
-              onClick={() => openImageViewer(imagePath.indexOf(imagePath[selectedImage]))}
-            />
-            {isViewerOpen && (
-             <ImageViewer
-               src={imagePath}
-               onClose={closeImageViewer}
-               currentIndex={currentImage}
-               backgroundStyle={{
-                 backgroundColor: "rgba(0,0,0,0.9)",
-                 zIndex: 1501,
-               }}
-             />
-            )}
-          </FlexBox>
+    <>
+      <Dialog
+        open={dialogOpen}
+        scroll="body"
+        onClose={toggleDialog}
+      >
+        <Login/>
+      </Dialog>
 
-          <FlexBox overflow="auto">
-            {imagePath.map((url, ind) => (
-              <FlexRowCenter
-                key={ind}
-                width={64}
-                height={64}
-                minWidth={64}
-                bgcolor="white"
-                border="1px solid"
-                borderRadius="10px"
-                ml={ind === 0 ? "auto" : 0}
-                style={{
-                  cursor: "pointer",
-                }}
-                onClick={handleImageClick(ind)}
-                mr={ind === imagePath.length - 1 ? "auto" : "10px"}
-                borderColor={
-                  selectedImage === ind ? "primary.main" : "grey.400"
-                }
-              >
-                <BazaarAvatar src={url} variant="square" height={40}/>
-              </FlexRowCenter>
-            ))}
-          </FlexBox>
-        </Grid>
+      <Box width="100%">
+        <Grid container spacing={3} justifyContent="space-around">
+          <Grid item md={6} xs={12} alignItems="center">
+            <FlexBox justifyContent="center" mb={6}>
+              <LazyImage
+                width={300}
+                alt={product.title}
+                height={300}
+                loading="eager"
+                objectFit="contain"
+                src={imagePath[selectedImage]}
+                onClick={() => openImageViewer(imagePath.indexOf(imagePath[selectedImage]))}
+              />
+              {isViewerOpen && (
+                <ImageViewer
+                  src={imagePath}
+                  onClose={closeImageViewer}
+                  currentIndex={currentImage}
+                  backgroundStyle={{
+                    backgroundColor: "rgba(0,0,0,0.9)",
+                    zIndex: 1501,
+                  }}
+                />
+              )}
+            </FlexBox>
 
-        <Grid item md={6} xs={12} alignItems="center">
-          <H1 mb={0}>{product.title}</H1>
-          <FlexBox alignItems="center" mb={2}>
-            <Small color={"grey.900"} >{new Date(product.updateTime).toDateString()}</Small>
-          </FlexBox>
-          <Box mb={3}>
-            <H2 color="primary.main" mb={0.5} lineHeight="1">
-              ${product.price.toFixed(2)}
-            </H2>
+            <FlexBox overflow="auto">
+              {imagePath.map((url, ind) => (
+                <FlexRowCenter
+                  key={ind}
+                  width={64}
+                  height={64}
+                  minWidth={64}
+                  bgcolor="white"
+                  border="1px solid"
+                  borderRadius="10px"
+                  ml={ind === 0 ? "auto" : 0}
+                  style={{
+                    cursor: "pointer",
+                  }}
+                  onClick={handleImageClick(ind)}
+                  mr={ind === imagePath.length - 1 ? "auto" : "10px"}
+                  borderColor={
+                    selectedImage === ind ? "primary.main" : "grey.400"
+                  }
+                >
+                  <BazaarAvatar src={url} variant="square" height={40}/>
+                </FlexRowCenter>
+              ))}
+            </FlexBox>
+          </Grid>
 
-            {/*<Box color="inherit">Stock Available</Box>*/}
-          </Box>
-          <FlexBox alignItems="center" mb={2}>
-            <Box>Category:</Box>
-            <H6 ml={1}>{product.category}</H6>
-          </FlexBox>
+          <Grid item md={6} xs={12} alignItems="center">
+            <H1 mb={0}>{product.title}</H1>
+            <FlexBox alignItems="center" mb={2}>
+              <Small color={"grey.900"}>{new Date(product.updateTime).toDateString()}</Small>
+            </FlexBox>
+            <Box mb={3}>
+              <H2 color="primary.main" mb={0.5} lineHeight="1">
+                ${product.price.toFixed(2)}
+              </H2>
 
-          <FlexBox alignItems="center" mb={2}>
-            <Box>Sold By:</Box>
-            {/*<Link href="/shops/fdfdsa">*/}
+              {/*<Box color="inherit">Stock Available</Box>*/}
+            </Box>
+            <FlexBox alignItems="center" mb={2}>
+              <Box>Category:</Box>
+              <H6 ml={1}>{product.category}</H6>
+            </FlexBox>
+
+            <FlexBox alignItems="center" mb={2}>
+              <Box>Sold By:</Box>
+              {/*<Link href="/shops/fdfdsa">*/}
               <a>
                 <H6 ml={1}>{product.username}</H6>
               </a>
-            {/*</Link>*/}
-            <Box mx={1} lineHeight="1">
-              <BazaarRating
-                color="warn"
-                fontSize="1.25rem"
-                value={product.sellerRating}
-                readOnly
-              />
+              {/*</Link>*/}
+              <Box mx={1} lineHeight="1">
+                <BazaarRating
+                  color="warn"
+                  fontSize="1.25rem"
+                  value={product.sellerRating}
+                  readOnly
+                />
+              </Box>
+            </FlexBox>
+
+            <FlexBox alignItems="center" mb={2}>
+              <Box>Email:</Box>
+              <H6 ml={1}>{product.userEmail}</H6>
+            </FlexBox>
+
+
+            <FlexBox alignItems="center" mb={2}>
+              Payment:
+              <H6 ml={1}>{product.paymentMethod}</H6>
+            </FlexBox>
+
+            <Box mb={2}>
+              <H3 mb={0}>Description:</H3>
+              <Box>
+                <H6>{product.description}</H6>
+              </Box>
             </Box>
-          </FlexBox>
 
-          <FlexBox alignItems="center" mb={2}>
-            <Box>Email:</Box>
-            <H6 ml={1}>{product.userEmail}</H6>
-          </FlexBox>
-
-
-          <FlexBox alignItems="center" mb={2}>
-            Payment:
-            <H6 ml={1}>{product.paymentMethod}</H6>
-          </FlexBox>
-
-          <Box mb={2}>
-            <H3 mb={0}>Description:</H3>
-            <Box>
-              <H6 >{product.description}</H6>
-            </Box>
-          </Box>
-
-          <FlexBox alignItems="center" my={3}>
-            <Box  lineHeight="1">
-              <BazaarButton
-                color="primary"
-                variant="outlined"
-                onClick={handleContact}
-                sx={{
-                  mb: 4.5,
-                  px: "1.75rem",
-                  height: 40,
-                }}
-              >
-                Contact seller
-              </BazaarButton>
-            </Box>
-            <Box mx={2} lineHeight="1">
-              <BazaarButton
-                color="primary"
-                variant="contained"
-                onClick={orderHandler}
-                sx={{
-                  mb: 4.5,
-                  px: "1.75rem",
-                  height: 40,
-                }}
-              >
-                Order
-              </BazaarButton>
-            </Box>
-          </FlexBox>
+            <FlexBox alignItems="center" my={3}>
+              <Box lineHeight="1">
+                <BazaarButton
+                  color="primary"
+                  variant="outlined"
+                  onClick={handleContact}
+                  sx={{
+                    mb: 4.5,
+                    px: "1.75rem",
+                    height: 40,
+                  }}
+                >
+                  Contact seller
+                </BazaarButton>
+              </Box>
+              <Box mx={2} lineHeight="1">
+                <BazaarButton
+                  color="primary"
+                  variant="contained"
+                  onClick={orderHandler}
+                  sx={{
+                    mb: 4.5,
+                    px: "1.75rem",
+                    height: 40,
+                  }}
+                >
+                  Order
+                </BazaarButton>
+              </Box>
+            </FlexBox>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </>
   );
 };
 
