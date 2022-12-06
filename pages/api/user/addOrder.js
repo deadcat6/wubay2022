edb0d5efb09ad263bc08a0e5c0482ac26dc778f6
deadcat6 <89941927+deadcat6@users.noreply.github.com>
2022-12-06 06:09:17 -0600

    publish tiny bug

--- pages/api/user/addOrder.js
+++ pages/api/user/addOrder.js
@@ -1,5 +1,5 @@
 import {addOrder} from '../../../firebase/firebaseSetUser.js';
-import {setTransaction} from "../../../firebase/firebaseSetProduct";
+import {setProductPublish, setTransaction} from "../../../firebase/firebaseSetProduct";
 import {newChat} from '../../../firebase/firebaseChats';
 
 async function handler(req, res) {
@@ -9,6 +9,7 @@ async function handler(req, res) {
     await addOrder(userId, productId);
     await setTransaction("Pending Payment",productId, userId)
     await newChat(userId, sellerId);
+    await setProductPublish(productId, false);
     res.status(201).json({message: 'success'});
   }
 }
